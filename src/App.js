fbc64c77175b0bc8516fc74d319a6b80be68a3d5
Khmilevoi <Khmilevoi@gmail.com>
2018-12-14 21:14:30 +0200

    3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components'
-
 import CardFront from "./components/front/card-front";
 import CardBack from "./components/back/card-back";
 
